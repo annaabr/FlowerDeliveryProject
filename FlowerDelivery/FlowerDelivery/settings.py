cb41d7f2408fc0b391ea6c885dfeb5a813089939
annaabr <annaabramyan2013@gmail.com>
2025-01-06 20:37:33 +0300

    done

--- FlowerDelivery/FlowerDelivery/settings.py
+++ FlowerDelivery/FlowerDelivery/settings.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-84pz(fg*oxui97qm(s0(8b2^5)*$w$8t20mv%+)dv@h&$tq$e#"
+SECRET_KEY = "SECRET_KEY"
 
 DEBUG = True
 
@@ -73,15 +73,16 @@ DATABASES = {
 
 # Теперь через google
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST =  'smtp.gmail.com' # 'smtp.your-email-provider.com'
+EMAIL_HOST =  'smtp.your-email-provider.com'  # Нужно заполнить
+
 EMAIL_PORT = 587 # Для TLS
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True # Для TLS
+
 # EMAIL_PORT = 465  # Для SSL
-# EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'annaabramyan2013@gmail.com' # 'your-email@example.com'
-EMAIL_HOST_PASSWORD = 'AnnaAbramyan19695616' # 'your-email-password'
+# EMAIL_USE_SSL = True # Для TLS
 
-
+EMAIL_HOST_USER = 'your-email@example.com'    # Нужно заполнить
+EMAIL_HOST_PASSWORD = 'your-email-password'   # Нужно заполнить
 
 # Конец секции с параметрами SMTP
 
@@ -96,14 +97,14 @@ COMMON_DICT = {
 
        # Время работы магазина:
        'start_time': '8:00',  # Начало рабочего дня
-       'end_time': '18:00',   # Конец рабочего дня
+       'end_time': '22:00',   # Конец рабочего дня
 
         # Временные интервалы для доставки:
        'delivery_time_intervals' : ['09:00 - 11:00', '11:00 - 13:00', '13:00 - 15:00', '15:00 - 17:00', '17:00 - 19:00','19:00 - 21:00']
    }
 
-TELEGRAM_BOT_TOKEN = '7103387978:AAHPys-AhKpYN2YD3YC8ua-i_xFNZpDDkk0'
-TELEGRAM_CHAT_ID = '708732931'
+TELEGRAM_BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+TELEGRAM_CHAT_ID = 'YOUR_TELEGRAM_CHAT_ID'
 
 # Конец секции с глобальными переменными этого проекта
 #-------------------------------------------------------------------------
